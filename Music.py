831dafadea2f4f46d915d08897a44eb5b9b3c368
VEXERAOP <98903684+VEXERAOP@users.noreply.github.com>
2022-02-12 13:59:35 +0530

    Update Music.py

--- Music.py
+++ Music.py
@@ -140,34 +140,21 @@ async def close_handler(client: PyTgCalls, chat_id: int):
         clear_queue(chat_id)
         
 async def yt_video(link):
-    proc = await asyncio.create_subprocess_exec(
-        "yt-dlp",
-        "-g",
-        "-f",
-        "best[height<=?720][width<=?1280]",
-        f"{link}",
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
+    stdout, stderr = await bash(
+        f'yt-dlp -g -f "best[height<=?720][width<=?1280]" {link}'
     )
-    stdout, stderr = await proc.communicate()
     if stdout:
-        return 1, stdout.decode().split("\n")[0]
-    else:
-        return 0, stderr.decode()
+        return 1, stdout
+    return 0, stderr
     
 
 async def yt_audio(link):
-    proc = await asyncio.create_subprocess_exec(
-        "yt-dlp",
-        "-g",
-        "-f",
-        "bestaudio",
-        f"{link}",
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
+    stdout, stderr = await bash(
+        f'yt-dlp -g -f "best[height<=?720][width<=?1280]" {link}'
     )
-    stdout, stderr = await proc.communicate()
     if stdout:
-        return 1, stdout.decode().split("\n")[0]
-    else:
-        return 0, stderr.decode()
+        return 1, stdout
+    return 0, stderr
+
+
+    
