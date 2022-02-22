5b745b669ccda7bb19773449d5c0e3b303c70fe8
VEXERAOP <98903684+VEXERAOP@users.noreply.github.com>
2022-02-22 10:33:50 +0530

    Update Music.py

--- Music.py
+++ Music.py
@@ -6,7 +6,7 @@ import asyncio
 import youtube_dl
 import yt_dlp
 from pytgcalls import StreamType
-from Py_DL import YouTube
+from Py-DL import YouTube
 from youtube_search import YoutubeSearch
 from pytgcalls import PyTgCalls, idle
 from pytgcalls.types import Update
